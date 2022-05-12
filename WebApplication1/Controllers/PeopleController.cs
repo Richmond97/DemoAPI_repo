ed9c84b0367adf7f5f95d4d72204e2e803444386
Richmond Yeboah <richmond.yeboah@angstromsports.com>
2022-05-12 12:50:15 +0100

    Project loaded

---
+++ WebApplication1/Controllers/PeopleController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DemoAPI.Models;
+
+namespace DemoAPI.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        List<Person> people = new List<Person>();
+        // GET api/people
+        public PeopleController()
+        {
+            people.Add(new Person { FirstName = "Tim", LastName = "Storm", Id = 1 });
+            people.Add(new Person { FirstName = "Sue", LastName = "Corey", Id = 2 });
+            people.Add(new Person { FirstName = "Bilbo", LastName = "Corey", Id = 3 });
+        }
+        public List<Person> Get()
+        {
+            return people;
+        }
+
+        // GET api/people/5
+        public Person Get(int id)
+        
+        {
+            var count = people.Count;
+            return people.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+
+        [Route("api/People/GetLastname")]
+        [HttpGet]
+        public List<string> GetLastname()
+        {
+            List<string> surnames = new List<string>();
+
+            foreach (var p in people)
+            {
+                surnames.Add(p.LastName);
+            }
+
+            return surnames;
+        }
+
+        // POST api/people
+        public void Post([FromBody] Person value)
+        {
+            people.Add((Person)value);  
+        }
+
+        // PUT api/people/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/people/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DemoAPI.Models;
+
+namespace DemoAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        List<Person> people = new List<Person>();
+        // GET api/values
+        public List<Person> Get()
+        {
+            people.Add(new Person { FirstName = "Tim", LastName = "Storm", Id = 1 });
+            people.Add(new Person { FirstName = "Sue", LastName = "Corey", Id = 2 });
+            people.Add(new Person { FirstName = "Bilbo", LastName = "Corey", Id = 3 });
+
+            return people;
+        }
+
+        // GET api/values/5
+        public Person Get(int id)
+        {
+           return people.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
